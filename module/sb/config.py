d2134934430e9e5aabc42979ac9607192d9fb3d8
SprintBook <74133119+SprintData@users.noreply.github.com>
2021-03-12 15:41:53 +0300

    Add files via upload

---
+++ module/sb/config.py
@@ -0,0 +1,8 @@
+PREFIX = 's*'
+
+
+COLOR_ERROR = 0xFF2B2B # Ваш Цвет без ""
+COLOR_WARN = 0xFFFF00 # Ваш Цвет без ""
+COLOR_GOOD = 0x008000 # Ваш Цвет без ""
+COPYRIGHT_TEXT = 'Все права защищены | 𝓐𝓿𝓪𝓴𝓾𝓶 𝓟𝓻𝓸𝓭𝓾𝓬𝓻𝓲𝓸𝓷'
+COPYRIGHT_ICON = 'https://cdn.discordapp.com/avatars/761899399218855947/c6608a8669526fcf8db9e24874c46c24.png?size=512'
